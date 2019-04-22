c41ce070dc32fa2f22ae605312ea74dd0d16a843
David SERGENT <david.sergent@emanet.fr>
2019-04-22 17:42:48 +0200

    Initial Commit

---
+++ Multilangues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Multilangues
+//
+//  Created by Snoopy on 22/04/2019.
+//  Copyright © 2019 EMANET. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
